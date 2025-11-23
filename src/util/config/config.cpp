35105f57617802d403c4dd73d698fcf4f9c302a5
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2025-11-23 20:56:17 +0200

    [d3d7] Sanitize err/warn log levels

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -1504,6 +1504,10 @@ namespace dxvk {
     { R"(\\MessiahD3D\.exe$)", {{
       { "d3d7.autoGenMipMaps",              "True" },
     }} },
+    /* Might and Magic IX / No One Lives Forever  */
+    { R"(\\lithtech\.exe$)", {{
+      { "d3d7.proxiedQueryInterface",       "True" },
+    }} },
 
   };
 
