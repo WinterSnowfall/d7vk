d55d3126a7dfeaa2ab9c74e52b9085b21b85bd89
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2025-11-26 11:50:23 +0200

    [d3d7] Refine how presentation intervals are handled

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -1529,6 +1529,10 @@ namespace dxvk {
     { R"(\\Sum\.exe$)", {{
       { "d3d9.maxFrameRate",                  "60" },
     }} },
+    /* Wizardry 8 - Broken input handling         */
+    { R"(\\Wiz8\.exe$)", {{
+      { "d3d9.maxFrameRate",                  "60" },
+    }} },
 
   };
 
