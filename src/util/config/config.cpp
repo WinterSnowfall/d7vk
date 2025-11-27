4efdca135284203ff831117fb210c2fe85d4e0d7
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2025-11-28 00:46:38 +0200

    [d3d7] Respect application set DDSCL_MULTITHREADED flags

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -1541,6 +1541,10 @@ namespace dxvk {
     { R"(\\edd\.exe$)", {{
       { "d3d7.forceProxiedPresent",         "True" },
     }} },
+    /* Silent Hunter II - Broken input handling   */
+    { R"(\\Silent Hunter.*\\(Sim|Shell(1)?)\.exe$)", {{
+      { "d3d9.maxFrameRate",                  "60" },
+    }} },
 
   };
 
