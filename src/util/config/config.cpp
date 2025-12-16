72d78ec65fc8e0b56b3f88bfba031610779df97e
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2025-12-16 22:48:18 +0200

    [d3d7] Fix device enumeration order and caps

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -1586,6 +1586,7 @@ namespace dxvk {
     /* Soulbringer - Uses legacy ddraw interfaces */
     { R"(\\SoulbringeVC(noeax)?\.exe$)", {{
       { "d3d7.forceProxiedPresent",         "True" },
+      { "d3d7.forceSWVPDevice",             "True" },
     }} },
 
   };
