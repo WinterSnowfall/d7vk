eba7605586835fc3b9d47751e2f59bb0bb244028
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2025-12-27 16:25:52 +0200

    [d3d6] Fix viewport, light and material updates

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -1599,6 +1599,10 @@ namespace dxvk {
     { R"(\\fallen\.exe$)", {{
       { "ddraw.ignoreExclusiveMode",        "True" },
     }} },
+    /* Redline - Fixes missing weapon mip maps    */
+    { R"(\\Redline\.exe$)", {{
+      { "d3d7.autoGenMipMaps",              "True" },
+    }} },
 
   };
 
