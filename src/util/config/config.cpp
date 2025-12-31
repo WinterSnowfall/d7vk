88ffe17ff2d5ad19b809a0decb163fe53ad7f3eb
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2025-12-31 22:56:38 +0200

    [d3d6] Implement more legacy interface wrappers

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -1626,6 +1626,10 @@ namespace dxvk {
     { R"(\\h&d\.exe$)", {{
       { "d3d9.allowDirectBufferMapping",   "False" },
     }} },
+    /* Dungeon Keeper 2                           */
+    { R"(\\DKII-DX\.exe$)", {{
+      { "ddraw.forceProxiedPresent",        "True" },
+    }} },
 
   };
 
