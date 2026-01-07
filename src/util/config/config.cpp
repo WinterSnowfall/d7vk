6d059b21007e82eb0a6f52f8bf1554ffc4feb912
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2026-01-07 22:03:13 +0200

    [meta] Move more data to common interfaces

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -1661,6 +1661,7 @@ namespace dxvk {
     /* Redline - Fixes missing weapon mip maps    */
     { R"(\\Redline\.exe$)", {{
       { "ddraw.autoGenMipMaps",             "True" },
+      { "ddraw.forceProxiedPresent",        "True" },
     }} },
     /* 3DMark 99 (Max) - Enables VSync by default *
      * (probably due to hardware and/or driver    *
@@ -1691,6 +1692,10 @@ namespace dxvk {
     /* FreeSpace 2 - Fixes main menu animations   */
     { R"(\\FS2\.exe$)", {{
       { "ddraw.alwaysDirtyMipMaps",         "True" },
+    }} },
+    /* Descent: FreeSpace - The Great War         */
+    { R"(\\FS\.exe$)", {{
+      { "ddraw.forceProxiedPresent",        "True" },
     }} },
 
   };
