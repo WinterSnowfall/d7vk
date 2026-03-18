3143a5daedac4af06c00860e1331ff280b0668ed
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2026-03-18 22:25:38 +0200

    [meta] Add a POW2 textures config option

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -1797,6 +1797,7 @@ namespace dxvk {
     /* Total Annihilation: Kingdoms               */
     { R"(\\KINGDOMS\.icd$)", {{
       { "ddraw.emulateFSAA",                "True" },
+      { "ddraw.forcePOW2Textures",          "True" },
     }} },
     /* Star Wars Episode I: Racer                 */
     { R"(\\SWEP1RCR\.exe$)", {{
