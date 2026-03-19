7f7ba64155351490d1d6fffec588f4e3ffa15559
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2026-03-19 19:23:16 +0200

    [meta] Don't advertise R3G3B2 support
    Co-authored-by: Konstantin Kuzov <master.nosferatu@gmail.com>

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -1456,13 +1456,17 @@ namespace dxvk {
     { R"(\\(Empire Earth|EE-AOC)\.exe$)", {{
       { "ddraw.forceProxiedPresent",        "True" },
     }} },
-    /* Etherlords                                 */
+    /* Etherlords                                 *
+     * Needs R3G3B2 support for text rendering    */
     { R"(\\Etherlords\.exe$)", {{
       { "ddraw.forceProxiedPresent",        "True" },
+      { "ddraw.supportR3G3B2",              "True" },
     }} },
-    /* Etherlords 2                               */
+    /* Etherlords 2                               *
+     * Needs R3G3B2 support for text rendering    */
     { R"(\\Etherlords2\.exe$)", {{
       { "ddraw.forceProxiedPresent",        "True" },
+      { "ddraw.supportR3G3B2",              "True" },
     }} },
     /* Evil Islands                               */
     { R"(\\Evil Islands\\game\.exe$)", {{
@@ -1854,6 +1858,10 @@ namespace dxvk {
     { R"(\\crusaders\.exe$)", {{
       { "ddraw.backBufferWriteBack",        "True" },
       { "ddraw.backBufferGuard",        "Disabled" },
+    }} },
+    /* DethKarz - fixes crash post intro playback */
+    { R"(\\Dethkarz\.exe$)", {{
+      { "ddraw.mask8BitModes",              "True" },
     }} },
 
     /**********************************************/
