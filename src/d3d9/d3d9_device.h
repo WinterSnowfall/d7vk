b8098d3a2ad8d29b146afc65d5011800ea13455f
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2026-04-01 14:52:30 +0300

    [d3d6/5] Implement legacy light attenuation

--- src/d3d9/d3d9_device.h
+++ src/d3d9/d3d9_device.h
@@ -86,6 +86,7 @@ namespace dxvk {
 
     FFColorKeyState,
     FFColorKey,
+    FFLegacyLightsState,
 
     SpecializationEntries,
   };
@@ -1001,6 +1002,8 @@ namespace dxvk {
 
     void UpdateColorKey();
 
+    void UpdateLegacyLightState();
+
     void BindFramebuffer();
 
     void BindViewportAndScissor();
@@ -1484,6 +1487,15 @@ namespace dxvk {
       return D3D_OK;
     }
 
+    HRESULT SetLegacyLightsState(bool legacyLightState) {
+      if (likely(m_useLegacyLights != legacyLightState)) {
+        m_dirty.set(D3D9DeviceDirtyFlag::FFLegacyLightsState);
+        m_useLegacyLights = legacyLightState;
+      }
+
+      return D3D_OK;
+    }
+
     HRESULT SetColorKey(DWORD colorKeyLow, DWORD colorKeyHigh) {
       if (likely(m_state.colorKeyLow != colorKeyLow || m_state.colorKeyHigh != colorKeyHigh)) {
         m_dirty.set(D3D9DeviceDirtyFlag::FFColorKey);
@@ -1697,6 +1709,8 @@ namespace dxvk {
 
     // D3D7 and earlier color key transparency state
     bool                            m_colorKeyEnabled  = false;
+    // D3D6 and earlier legacy light model state
+    bool                            m_useLegacyLights  = false;
 
     // the enablement of below features is tracked independently
     // of render states both due to complexity and to avoid
