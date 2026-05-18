6d7833927c204f2c32104b1761545cc597bf4aad
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2026-05-18 13:34:19 +0300

    [d3d7] Extend legacy discard behavior logic

--- src/ddraw/ddraw/ddraw_surface.cpp
+++ src/ddraw/ddraw/ddraw_surface.cpp
@@ -607,7 +607,7 @@ namespace dxvk {
 
         if (unlikely(rt != nullptr)) {
           Logger::debug("DDrawSurface::Flip: Presenting from DDraw RT");
-          if (unlikely(m_commonIntf->GetOptions()->uploadFrontBuffer))
+          if (unlikely(m_commonIntf->GetOptions()->emulateFrontBuffer))
             InitializeOrUploadD3D9();
           rt->InitializeOrUploadD3D9();
           d3d9Device->Present(NULL, NULL, NULL, NULL);
@@ -616,7 +616,7 @@ namespace dxvk {
       }
 
       if (likely(m_nextFlippable != nullptr)) {
-        if (unlikely(m_commonIntf->GetOptions()->uploadFrontBuffer))
+        if (unlikely(m_commonIntf->GetOptions()->emulateFrontBuffer))
           InitializeOrUploadD3D9();
         m_nextFlippable->InitializeOrUploadD3D9();
       } else {
