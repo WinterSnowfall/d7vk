4b4ca30fe1e23a0864cd85d699532a64f7ada978
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2026-08-04 16:44:42 +0300

    [meta] Optimize caps struct copies

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -1494,6 +1494,7 @@ namespace dxvk {
     /* Empire Earth / Art of Conquest             *
      * Works around in-game flickering            */
     { R"(\\(Empire Earth|EE-AOC)\.exe$)", {{
+      { "d3d9.cachedWriteOnlyBuffers",      "True" },
       { "ddraw.forceLegacyPresent",         "True" },
       { "ddraw.legacyPresentGuard",       "Strict" },
     }} },
